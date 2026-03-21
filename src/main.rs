a7e15663888cb127e8bef72eca813193af0eaab2
ConstintCreations <ethanglenn1208@gmail.com>
2026-03-20 19:10:32 -0500

    File picking with rfd

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use eframe::egui;
-struct ImageConvertor {}
+struct ImageConvertor {
+    input_path: Option<String>,
+}
 
 impl ImageConvertor {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+        Self {
+            input_path: None,
+        }
     }
 }
 
 impl eframe::App for ImageConvertor {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Image Convertor!");
+            ui.heading("Image Convertor");
+            ui.add_space(10.0);
+            if ui.button("Select an Image to Convert").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.input_path = Some(path.display().to_string());
+                }
+            } 
+
+            ui.label(format!("Selected Image: {:?}", self.input_path));
+
+            if ui.button("Covert").clicked() {
+                
+            }
         });
     }
 }
